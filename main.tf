dbb940b08e683a7e4958870063edd0c0d3a9e8c5
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-11 12:39:59 +0530

    updated code

--- main.tf
+++ main.tf
@@ -1,59 +1,24 @@
 resource "aws_lb" "main" {
-  name               = local.lb_name
+  name               = "test-lb-tf"
   internal           = var.internal
   load_balancer_type = var.lb_type
   security_groups    = [aws_security_group.main.id]
   subnets            = var.subnets
-  tags               = merge(local.tags, { Name = "${var.env}-alb" })
-}
-
-resource "aws_lb_listener" "main" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = var.internal ? 80 : 443
-  protocol          = var.internal ? "HTTP" : "HTTPS"
-  ssl_policy        = var.internal ? null : "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.internal ? null : var.acm_certificate_arn
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "ERROR"
-      status_code  = "404"
-    }
-  }
-}
-
-resource "aws_lb_listener" "frontend" {
-  count             = var.internal ? 0 : 1
-  load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
+  tags = merge(local.tags, { Name = "${var.env}-alb"})
 }
 
 resource "aws_security_group" "main" {
   name        = local.sg_name
   description = local.sg_name
   vpc_id      = var.vpc_id
-  tags        = merge(local.tags, { Name = local.sg_name })
+  tags = merge(local.tags, {Name = local.sg_name})
 
   ingress {
-    description = "APP"
-    from_port   = var.sg_port
-    to_port     = var.sg_port
-    protocol    = "tcp"
-    cidr_blocks = var.sg_ingress_cidr
+    description      = "APP"
+    from_port        = var.sg_port
+    to_port          = var.sg_port
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_ingress_cidr
   }
 
   egress {
@@ -63,14 +28,5 @@ resource "aws_security_group" "main" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-}
 
-resource "aws_security_group_rule" "frontend" {
-  count             = var.internal ? 0 : 1
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = var.sg_ingress_cidr
-  security_group_id = aws_security_group.main.id
 }
