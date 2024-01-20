3ec22aa034c7ee792aee407de10343f1fbabdeae
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-20 11:01:59 +0530

    updated code

--- main.tf
+++ main.tf
@@ -9,8 +9,10 @@ resource "aws_lb" "main" {
 
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
+  port              = var.internal ? 80 : 443
+  protocol          = var.internal ? "HTTP" : "HTTPS"
+  #ssl_policy        = var.internal ? null : "ELBSecurityPolicy-2016-08"
+  #certificate_arn   = var.internal ? null : var.acm_certificate_arn
 
   default_action {
     type = "fixed-response"
@@ -22,6 +24,24 @@ resource "aws_lb_listener" "main" {
     }
   }
 }
+
+resource "aws_lb_listener" "frontend" {
+  count             = var.internal ? 0 : 1
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 
 resource "aws_security_group" "main" {
   name        = local.sg_name
@@ -44,4 +64,14 @@ resource "aws_security_group" "main" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+}
+
+resource "aws_security_group_rule" "frontend" {
+  count             = var.internal ? 0 : 1
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = var.sg_ingress_cidr
+  security_group_id = aws_security_group.main.id
 }
