1d14ebeefddfec6d86a4d5b90c1d9a11df6a7641
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-25 10:01:49 +0530

    updated code

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "aws_lb" "main" {
   load_balancer_type = var.lb_type
   security_groups    = [aws_security_group.main.id]
   subnets            = var.subnets
+  
   tags = merge(local.tags, {Name = "${var.env}-alb"})
 }
 
