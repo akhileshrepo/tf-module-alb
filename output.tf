31d75201f986f84c2bf74a7827c7f4db0dc793fa
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-27 11:15:06 +0530

    updated code

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "alb" {
   value = aws_lb.main
 }
+
+output "listener" {
+  value = aws_lb_listener.main
+}
--- vars.tf
+++ vars.tf
@@ -6,3 +6,4 @@ variable "vpc_id" {}
 variable "sg_ingress_cidr" {}
 variable "sg_port" {}
 variable "subnets" {}
+variable "acm_certificate_arn" {}
