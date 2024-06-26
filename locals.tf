52fc66293d01377cfb35f327121264f050d12aed
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-26 16:22:08 +0530

    updated code

--- locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  tags    = merge(var.tags, { tf-module-name = "alb" }, { env = var.env })
-  sg_name = var.internal ? "${var.env}-alb-internal-sg" : "${var.env}-alb-public-sg" # NO NEED OF THIS
-  lb_name = var.internal ? "${var.env}-alb-internal" : "${var.env}-alb-public"
-}
--- output.tf
+++
@@ -1,7 +0,0 @@
-output "alb" {
-  value = aws_lb.main
-}
-
-output "listener" {
-  value = aws_lb_listener.main
-}
