4f91ae9482e88a6828d226e24bfb1fc2eefc539c
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 19:34:53 +0530

    updated code

--- main.tf
+++ main.tf
@@ -4,8 +4,7 @@ resource "aws_lb" "main" {
   load_balancer_type = var.lb_type
   security_groups    = [aws_security_group.main.id]
   subnets            = var.subnets
-
-  tags = merge(local.tags, { Name = "${var.env}-alb"})
+  tags               = merge(local.tags, { Name = "${var.env}-alb" })
 }
 
 resource "aws_lb_listener" "main" {
@@ -24,18 +23,17 @@ resource "aws_lb_listener" "main" {
   }
 }
 
-
 resource "aws_security_group" "main" {
   name        = local.sg_name
   description = local.sg_name
   vpc_id      = var.vpc_id
-  tags = merge(local.tags, { Name = local.sg_name})
+  tags        = merge(local.tags, { Name = local.sg_name })
 
   ingress {
     description = "APP"
-    from_port = var.sg_port
-    to_port = var.sg_port
-    protocol = "tcp"
+    from_port   = var.sg_port
+    to_port     = var.sg_port
+    protocol    = "tcp"
     cidr_blocks = var.sg_ingress_cidr
   }
 
@@ -46,5 +44,4 @@ resource "aws_security_group" "main" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
 }
--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "tags" {}
 variable "env" {}
-variable "internal" {}
 variable "lb_type" {}
+variable "internal" {}
 variable "vpc_id" {}
 variable "sg_ingress_cidr" {}
 variable "sg_port" {}
